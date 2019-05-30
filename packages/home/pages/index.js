8b6c1ca061c01ef9acecb44e4397392b4a6923e5
imranweb <imranweb97@gmail.com>
2019-05-30 14:11:25 +0530

    Testing CI

--- packages/home/pages/index.js
+++ packages/home/pages/index.js
@@ -4,10 +4,12 @@ import React from 'react';
 //import {Layout, Button} from 'shared';
 //import ButtonFlow from 'shared/ButtonFlow';
 const  prefix = process.NODE_ENV == 'development' ? 'http://localhost:5003' : 'https://home-app.imranweb.now.sh';
+const topBanner = `${prefix}/static/banner.png`;
 const image = `${prefix}/static/home.png`;
 const Index = (props) => (
   <div>
      <h1>Home Page changed</h1>
+     <img src={topBanner} width="100%" />
     <img src={image} width="100%" />
   </div>
 )
