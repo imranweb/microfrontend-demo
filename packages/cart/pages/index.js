0085e762d74306fd82a5db29e56a66903fc38515
imranweb <imranweb97@gmail.com>
2019-06-04 14:47:52 +0530

    Fixed checkout button

--- packages/cart/pages/index.js
+++ packages/cart/pages/index.js
@@ -3,7 +3,6 @@ const image = `${prefix}/static/cart.png`;
 
 const onClickHandler = () => {
     console.log("%c%cCheckout button not working", "background:black ; color: white", "color: red; font-size:25px");
-    
    // window.location.href =  '/checkout';
 }
 export default () => (
