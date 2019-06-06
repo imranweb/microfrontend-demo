09f7d27ac3a78b82faee553fc7536d1bfd45488d
imranweb <imranweb97@gmail.com>
2019-06-06 16:44:48 +0530

    Fixed checkout button

--- packages/cart/pages/index.js
+++ packages/cart/pages/index.js
@@ -2,12 +2,12 @@ const  prefix = process.env.NODE_ENV == 'development' ? 'http://localhost:5002' 
 const image = `${prefix}/static/cart.png`;
 
 const onClickHandler = () => {
-    //console.log("%c%cPlease fix me!!", "background:black ; color: white", "color: red; font-size:25px");
-    window.location.href =  '/checkout';
+    console.log("%c%cPlease fix me!!", "background:black ; color: white", "color: red; font-size:25px");
+    //window.location.href =  '/checkout';
 }
 export default () => (
     <div>
-       {/* <h1>Cart Page Modified 22 </h1> */}
+       {/* <h1>Cart Page Modified </h1> */}
        <img width="100%" src={image} useMap="#image-map" />
        <map name="image-map">
             <area target="" alt="" title="" onClick={onClickHandler} coords="1491,470,1062,417" shape="rect" />
